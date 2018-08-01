c4eb4627f1262568ac32af048e46b093d1f3b61d
Lane, Derek <derek.lane@elekta.com>
2018-08-01 15:22:51 -0500

    Moved worklist service and contract to proper folder

---
+++ Worklist/Worklist.Contracts/IWorklistManager.fs
@@ -0,0 +1,23 @@
+﻿namespace Worklist.Contracts
+
+open System.ServiceModel
+
+open FsServiceFramework
+
+[<ServiceContract>]
+[<IntranetPolicy>]
+type IWorklistManager =
+    [<OperationContract>] abstract GetWorklistForStaff: staffId:int -> WorklistItems
+    [<OperationContract>] abstract CompleteWorklistItem: itemId:int -> unit
+
+[<ServiceContract>]
+[<ComponentPolicy>]
+type IWorklistEngine = 
+    [<OperationContract>] abstract GetWorklistForStaff: staffId:int -> WorklistItems
+    [<OperationContract>] abstract CompleteWorklistItem: itemId:int -> unit
+
+[<ServiceContract>]
+[<ComponentPolicy>]
+type IWorklistDataAccess = 
+    [<OperationContract>] abstract GetWorklistForStaff : staffId:int -> WorklistItems
+    [<OperationContract>] abstract CompleteWorklistItem: itemId:int -> unit
---
+++ Worklist/Worklist.Contracts/WorklistManagerData.fs
@@ -0,0 +1,17 @@
+﻿namespace Worklist.Contracts
+
+open System
+open System.Runtime.Serialization
+
+[<DataContract>]
+[<CLIMutable>]
+type WorklistItem = {
+    [<DataMember>] ImageAcquired : DateTime
+    [<DataMember>] ApprovedOn : DateTime option }
+
+
+[<DataContract>]
+[<CLIMutable>]
+type WorklistItems = { 
+    [<DataMember>] StaffId : int
+    [<DataMember>] AllStaffWorklistItems : WorklistItem list }
