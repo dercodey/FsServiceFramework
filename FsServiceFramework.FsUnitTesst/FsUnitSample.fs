c6d56ec5a1c2f77978cad88d266ee7ced7b94c0c
Derek Lane <dglane001@hotmail.com>
2018-08-06 14:37:49 -0500

    fixing references

--- FsServiceFramework.FsUnitTesst/FsUnitSample.fs
+++ FsServiceFramework.FsUnitTesst/FsUnitSample.fs
@@ -4,6 +4,10 @@ open Microsoft.VisualStudio.TestTools.UnitTesting
 open FsUnit.MsTest
 open NHamcrest.Core
 
+open FsServiceFramework
+open Trending.Contracts
+open Trending.Services
+
 type LightBulb(state) =
    member x.On = state
    override x.ToString() =
@@ -14,6 +18,21 @@ type LightBulb(state) =
 [<TestClass>] 
 type ``Given a LightBulb that has had its state set to true`` ()=
    let lightBulb = new LightBulb(true)
+   let container = Hosting.createHostContainer()
+   [<ClassInitialize>] member test.
+     ``set up hosting services.`` () = 
+        container 
+        |> Hosting.registerService<ITrendingManager, TrendingManagerService>
+        |> Hosting.registerService<ITrendingEngine, TrendingEngineService>
+        |> Hosting.registerService<ITrendingDataAccess, TrendingDataAccess>
+        |> Hosting.registerFunction<ITrendCalculationFunction, TrendCalculation>
+        |> Hosting.registerRepository<int, SiteTrendingSeries>
+        |> Hosting.startServices 
+
+   [<ClassCleanup>] member test.
+     ``stop hosting services`` () =
+        container
+        |> Hosting.stopServices 
 
    [<TestMethod>] member test.
     ``when I ask whether it is On it answers true.`` ()=
