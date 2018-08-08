ca7462f580c181fc728740f39f4f2bfda637fe02
Derek Lane <dglane001@hotmail.com>
2018-08-08 17:24:44 -0500

    set up test repository as part of class initialization

--- FsServiceFramework.FsUnitTesst/FsUnitSample.fs
+++ FsServiceFramework.FsUnitTesst/FsUnitSample.fs
@@ -11,31 +11,35 @@ open Trending.Services
 [<TestClass>] 
 type ``test trending manager as example service`` () =
     let container = Hosting.createHostContainer()
-    let seriesId = 1    // seriesId for testing
+
+    // construct a repository for test data
+    let repository = 
+        VolatileRepository<int, SiteTrendingSeries>(fun sts -> sts.Id) 
+            :> IRepository<int, SiteTrendingSeries>
+
+    // seriesId for testing
+    let seriesId = 1    
+
+    // add first test record
+    do repository.Create 
+        { Id=seriesId;
+            Label=seriesId.ToString();
+            Protocol={ Algorithm = "trend"; Tolerance = 1.0 };
+            SeriesItems = [ { AllResults = []; SelectedResult = {Label=""; Matrix=[|1.0;0.0;0.0;0.0;1.0;0.0;0.0;0.0;1.0;0.0;0.0;0.0;1.0;0.0;0.0;0.0;|]} };
+                            { AllResults = []; SelectedResult = {Label=""; Matrix=[|1.0;0.0;0.0;0.0;1.0;0.0;0.0;0.0;1.0;0.0;0.0;0.0;1.0;0.0;0.0;0.0;|]} } ];
+            Shift = [| 1.0; 2.0; 3.0 |]; } |> ignore
+
+    // add second (dummy) test record
+    do repository.Create 
+        { Id=seriesId+1;
+            Label=(seriesId+1).ToString();
+            Protocol={ Algorithm = "trend"; Tolerance = 1.0 };
+            SeriesItems = [ { AllResults = []; SelectedResult = {Label=""; Matrix=[|1.0;0.0;0.0;0.0;1.0;0.0;0.0;0.0;1.0;0.0;0.0;0.0;1.0;0.0;0.0;0.0;|]} };
+                            { AllResults = []; SelectedResult = {Label=""; Matrix=[|1.0;0.0;0.0;0.0;1.0;0.0;0.0;0.0;1.0;0.0;0.0;0.0;1.0;0.0;0.0;0.0;|]} } ];
+            Shift = [| 1.0; 2.0; 3.0 |]; } |> ignore
     
     [<TestInitialize>] 
     member ___.``set up hosting services.`` () = 
-        // construct a repository for test data
-        let repository = 
-            VolatileRepository<int, SiteTrendingSeries>(fun sts -> sts.Id) 
-                :> IRepository<int, SiteTrendingSeries>
-
-        // add a single test record
-        repository.Create 
-            { Id=seriesId;
-                Label=seriesId.ToString();
-                Protocol={ Algorithm = "trend"; Tolerance = 1.0 };
-                SeriesItems = [ { AllResults = []; SelectedResult = {Label=""; Matrix=[|1.0;0.0;0.0;0.0;1.0;0.0;0.0;0.0;1.0;0.0;0.0;0.0;1.0;0.0;0.0;0.0;|]} };
-                                { AllResults = []; SelectedResult = {Label=""; Matrix=[|1.0;0.0;0.0;0.0;1.0;0.0;0.0;0.0;1.0;0.0;0.0;0.0;1.0;0.0;0.0;0.0;|]} } ];
-                Shift = [| 1.0; 2.0; 3.0 |]; } |> ignore
-
-        repository.Create 
-            { Id=seriesId+1;
-                Label=(seriesId+1).ToString();
-                Protocol={ Algorithm = "trend"; Tolerance = 1.0 };
-                SeriesItems = [ { AllResults = []; SelectedResult = {Label=""; Matrix=[|1.0;0.0;0.0;0.0;1.0;0.0;0.0;0.0;1.0;0.0;0.0;0.0;1.0;0.0;0.0;0.0;|]} };
-                                { AllResults = []; SelectedResult = {Label=""; Matrix=[|1.0;0.0;0.0;0.0;1.0;0.0;0.0;0.0;1.0;0.0;0.0;0.0;1.0;0.0;0.0;0.0;|]} } ];
-                Shift = [| 1.0; 2.0; 3.0 |]; } |> ignore
 
         // register services and IoC container
         container 
