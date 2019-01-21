53e864e79ca49ff8fadcbf00a22659a6c0a01ee0
Derek Lane <dglane001@gmail.com>
2019-01-21 17:04:02 -0600

    adding import stub with simple image data contract

---
+++ Import.Contracts/Library1.fs
@@ -0,0 +1,38 @@
+﻿namespace Import.Contracts
+
+open System
+open System.ServiceModel
+open System.Runtime.Serialization
+
+open FsServiceFramework
+
+[<DataContract>]
+type ImportImage() =
+
+    [<DataMember(EmitDefaultValue=false, IsRequired=true)>] 
+    member val Label = String.Empty with get, set
+
+    [<DataMember(EmitDefaultValue=false, IsRequired=true)>] 
+    member val Width = 0 with get, set
+
+    [<DataMember(EmitDefaultValue=false, IsRequired=true)>] 
+    member val Height = 0 with get, set
+
+    [<DataMember(EmitDefaultValue=false, IsRequired=true)>] 
+    member val Pixels = [| 0uy |] with get, set
+
+[<ServiceContract>]
+[<DicomPolicy>]
+type IImportManager =
+    [<OperationContract>] abstract ImportPortalImage: inImage:ImportImage -> int
+    [<OperationContract>] abstract ImportSpatialRegistration: sro:float[] -> int
+
+[<ServiceContract>]
+[<ComponentPolicy>]
+type IImportEngine = 
+     [<OperationContract>] abstract ImportPortalImage: inImage:ImportImage -> int
+
+[<ServiceContract>]
+[<ComponentPolicy>]
+type IImageDataAccess = 
+    [<OperationContract>] abstract StoreImage: inImage:ImportImage -> int
