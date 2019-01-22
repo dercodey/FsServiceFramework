32b203767e2f6e05df4ef37a6a091155a4fe9a1f
Derek Lane <dglane001@gmail.com>
2019-01-21 19:50:22 -0600

    consolidating contracts

--- Trending/Trending.Contracts/Trending.Contracts.fs
+++ Trending/Trending.Contracts/Trending.Contracts.fs
@@ -1,7 +1,11 @@
 ﻿namespace Trending.Contracts
 
-open System.Runtime.Serialization
 open System
+open System.Runtime.Serialization
+open System.ServiceModel
+
+open FsServiceFramework
+
 
 [<DataContract>]
 [<CLIMutable>]
@@ -57,3 +61,26 @@ type SiteTrendingSeries() =
 
     [<DataMember>] 
     member val Shift = [|0.0; 0.0; 0.0|] with get, set
+
+
+[<ServiceContract>]
+[<IntranetPolicy>]
+type ITrendingManager =
+    [<OperationContract>] abstract GetSeries: siteId:int -> SiteTrendingSeries
+    [<OperationContract>] abstract UpdateSeries : series:SiteTrendingSeries -> SiteTrendingSeries
+    [<OperationContract>] abstract UpdateSiteOffset : series:SiteTrendingSeries -> int
+
+[<ServiceContract>]
+[<ComponentPolicy>]
+type ITrendingEngine = 
+    [<OperationContract>] abstract CalculateTrendForSeries : series:SiteTrendingSeries -> SiteTrendingSeries
+    [<OperationContract>] abstract UpdateSiteOffset : series:SiteTrendingSeries -> int
+
+type ITrendCalculationFunction =
+    abstract Calculate : SiteTrendingSeries -> double
+
+[<ServiceContract>]
+[<ComponentPolicy>]
+type ITrendingDataAccess = 
+    [<OperationContract>] abstract GetTrendingSeries : seriesId:int -> SiteTrendingSeries
+    [<OperationContract>] abstract UpdateTrendingSeries : series:SiteTrendingSeries -> unit
--- Worklist/Worklist.Contracts/Worklist.Contracts.fs
+++ Worklist/Worklist.Contracts/Worklist.Contracts.fs
@@ -1,8 +1,22 @@
 ﻿namespace Worklist.Contracts
 
 open System.ServiceModel
+open System
+open System.Runtime.Serialization
 
 open FsServiceFramework
+
+[<DataContract>]
+[<CLIMutable>]
+type WorklistItem = {
+    [<DataMember>] ImageAcquired : DateTime
+    [<DataMember>] ApprovedOn : DateTime option }
+
+[<DataContract>]
+[<CLIMutable>]
+type WorklistItems = { 
+    [<DataMember>] StaffId : int
+    [<DataMember>] AllStaffWorklistItems : WorklistItem list }
 
 [<ServiceContract>]
 [<IntranetPolicy>]
